173bc47568eb401b6a87b58ece08bdc72ff6d6d6
Lidia <97204663+Lilly0806@users.noreply.github.com>
2025-11-06 11:23:19 -0300

    Add files via upload

---
+++ src/html/pagina-servicos/recursos/js/scripts.js
@@ -0,0 +1,9 @@
+// Botão para abrir/fechar menu no celular
+const menuBtn = document.getElementById('menu-btn');
+const nav = document.getElementById('nav');
+
+menuBtn.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
+
+
