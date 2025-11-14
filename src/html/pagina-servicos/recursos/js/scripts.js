c88ff0d772fc8977780987567826e21e04d25b29
Lidia <97204663+Lilly0806@users.noreply.github.com>
2025-11-14 12:21:59 -0300

    Delete src/html/pagina-servicos/recursos directory

--- src/html/pagina-servicos/recursos/js/scripts.js
+++
@@ -1,9 +0,0 @@
-// Botão para abrir/fechar menu no celular
-const menuBtn = document.getElementById('menu-btn');
-const nav = document.getElementById('nav');
-
-menuBtn.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
-
-
